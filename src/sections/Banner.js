cc860de6de68fc3c4425c562583e79af69e5d4eb
sathyaseelan <iamsathyaseelan@gmail.com>
2022-12-01 23:03:03 +0530

    Video recording added

--- src/sections/Banner.js
+++ src/sections/Banner.js
@@ -29,7 +29,7 @@ export default function Banner() {
             </div>
           </div>
           <div
-              className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+              className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-50rem)]">
             <svg
                 className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
                 viewBox="0 0 1155 678"
